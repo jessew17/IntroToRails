dc0234fb89ceb3b8852282b9db7a4016ac995d66
jwiebe99@academic.rrc.ca <vagrant@Base-Box>
2018-10-11 20:09:00 +0000

    First try completed seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Seeding characters
+200.times do 
+  Character.create (
+    name: Faker::ElderScrolls.name.unique,
+    race: Faker::ElderScrolls.race
+  )
+end
+
+#Seeding Creatures
+for c in Faker::ElderScrolls.creature do
+  Creature.create(type: c)
+end
+
+#Seeding Dragons
+for d in Faker::ElderScrolls.dragon do
+  Creature.create(type: d)
+end
+
+#Seeding Scenarios
+for r in Faker::ElderScrolls.region do
+  Scenario.create(
+    region: r,
+    city: Faker::ElderScrolls.city.unique,
+    difficulty: Faker::Number.within(0..1)
+    )
+end
+
+#Seeding Stats
+for r in Faker::ElderScrolls.race do
+  Stat.create(
+    type: r,
+    hp: Faker::Number.between(50..100),
+    attack: Faker::Number.between(6..17),
+    armor: Faker::Number.between(5..25)
+    )
+end
+
+for c in Faker::ElderScrolls.creature do
+  Stat.create(
+    type: c,
+    hp: Faker::Number.between(50..100),
+    attack: Faker::Number.between(1..9),
+    armor: Faker::Number.between(0..5)
+    )
+end
+
+for d in Faker::ElderScrolls.dragon do
+  Stat.create(
+    type: d,
+    hp: Faker::Number.between(40..75),
+    attack: Faker::Number.between(3..12),
+    armor: Faker::Number.between(0..10)
+    )
+end
